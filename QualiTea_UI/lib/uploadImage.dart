2ef4731a4e7418cdcfbd7bfd0683144fac569e35
Parami Hansika <phansikak@gmail.com>
2023-03-17 15:05:26 +0530

    fix the issue in image selection

--- QualiTea_UI/lib/uploadImage.dart
+++ QualiTea_UI/lib/uploadImage.dart
@@ -3,94 +3,103 @@ import 'dart:io';
 import 'dart:convert';
 import 'package:image_picker/image_picker.dart';
 
-
 class uploadImage extends StatefulWidget {
   uploadImage() : super();
 
   final String title = "Upload Image";
 
   uploadImageDemo createState() => uploadImageDemo();
-  
 }
 
-
 class uploadImageDemo extends State<uploadImage> {
-
-  File? SelectedImage;
+  String? SelectedImage;
   String? message = "";
 
-  uploadImage (){
-    
+  uploadImage() {}
+
+  Future getImage() async {
+    final pickedImage =
+        await ImagePicker().getImage(source: ImageSource.gallery);
+    SelectedImage = pickedImage!.path;
+    setState(() {});
   }
 
-  Future getImage() async{
-   final pickedImage = await ImagePicker().getImage(source: ImageSource.gallery);
-   SelectedImage = File(pickedImage!.path);
-   setState(() {
-     
-   });
-  }
-
-  Widget build (BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: AppBar(
         title: Text(widget.title),
-        backgroundColor:Color.fromARGB(255, 9, 58, 7),
+        backgroundColor: Color.fromARGB(255, 9, 58, 7),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             SelectedImage == null
-            ? Text("Please upload the image you wish to get information of.")
-            : Image.file(SelectedImage!),
+                ? Text(
+                    "Please upload the image you wish to get information of.")
+                : Container(
+                  height: 260.0,
+                  width: 260.0,
+                  child: Image.network(SelectedImage!),
+                ),
             Padding(
-          padding: EdgeInsets.only(top: 80.0),
-          child: TextButton.icon(
-            style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 9, 58, 7),
+              padding: EdgeInsets.only(top: 50.0),
+              child: TextButton.icon(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    Color.fromARGB(255, 9, 58, 7),
+                  ),
+                ),
+                onPressed: () {},
+                icon: Icon(Icons.upload_file, color: Colors.white),
+                label: Text(
+                  "Upload Image",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ),
-              onPressed: () {},
-              icon: Icon(Icons.upload_file,color: Colors.white),
-              label: Text("Upload Image",
-              style: TextStyle(
-                color: Colors.white,
-              ),),),),
-              Padding(
-                padding: EdgeInsets.all(35.0),
-                child: Column(
-                  children: [
-              SizedBox(height: 30),
-              Text(
-                "Our app will determine the relevant tea grade and the price range of the leaf images uploaded by you.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.black,
-                ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(50.0),
+              child: Column(
+                children: [
+                  SizedBox(height: 30),
+                   Container(
+                  color: Color.fromARGB(255, 195, 255, 160),
+                  child: Text(
+                    "Our app will determine the relevant tea grade and the price range of the leaf images uploaded by you.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                   ),
+                  SizedBox(height: 20),
+                  Container(
+                  color: Color.fromARGB(255, 195, 255, 160),
+                  child: Text(
+                    "We will also provide you with additional information about your images based on our system's capabilities.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  ),
+                ],
               ),
-              SizedBox(height: 20),
-              Text(
-                "We will also provide you with additional information about your images based on our system's capabilities.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.black,
-                ),
-              ),
-          ],
-        ),
-       ),
+            ),
           ],
         ),
       ),
-       floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton(
         onPressed: getImage,
         backgroundColor: Color.fromARGB(255, 9, 58, 7),
-        child: Icon(Icons.add_a_photo ,),
-        // foregroundColor: Color.fromARGB(255, 9, 58, 7),
+        child: Icon(
+          Icons.add_a_photo,
         ),
+        // foregroundColor: Color.fromARGB(255, 9, 58, 7),
+      ),
     );
   }
 }
-  
--- QualiTea_UI/lib/welcomeScreen.dart
+++ QualiTea_UI/lib/welcomeScreen.dart
@@ -1,110 +1,134 @@
 import 'package:flutter/material.dart';
 import 'package:testapp/uploadImage.dart';
 
-class WelcomeScreen extends StatelessWidget{
-  Widget build (BuildContext context){  
+class WelcomeScreen extends StatelessWidget {
+  Widget build(BuildContext context) {
     return Material(
       child: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         child: Stack(
           children: [
-            Stack(children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 9, 58, 7),
+            Stack(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 9, 58, 7),
+                  ),
                 ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 1.6,
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 195, 255, 160),
-                  borderRadius: 
-                  BorderRadius.only(bottomRight: Radius.circular(70)),  
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 1.6,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 195, 255, 160),
+                    borderRadius:
+                        BorderRadius.only(bottomRight: Radius.circular(70)),
+                  ),
                 ),
-                child: Center(child: Image.asset("lib/images/logo.png", scale: 0.8,),
-              ),
-              ),
-            ],
+                // Container(
+                //   constraints: BoxConstraints(
+                //     maxHeight: MediaQuery.of(context).size.height / 8,
+                //     maxWidth: MediaQuery.of(context).size.width,
+                //   ),
+                //   child: Align(
+                //     alignment: Alignment.topCenter,
+                //     child: SizedBox(
+                //       child: Center(
+                //         child: Image.asset(
+                //           "lib/images/logo.png",
+                //           width: 350, 
+                //           height: 350,
+                //           scale: 1.2,
+                //         ),
+                //       ),
+                //     ),
+                //   ),
+                // ),
+                Padding(
+                  padding: EdgeInsets.all(120),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 3,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("lib/images/tea.png"),
+                        // fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 2.666,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 195, 255, 160),
+                  )),
             ),
             Align(
               alignment: Alignment.bottomCenter,
               child: Container(
                 width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 2.666 ,
+                height: MediaQuery.of(context).size.height / 2.666,
                 decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 195, 255, 160),
-                  
-                  )
-                ),
-              ),
-              Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 2.666 ,
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 9, 58, 7),
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(70),)
-                  ),
-                  child: Column(
-                    children: [
-                      SizedBox(height: 12),
-                      Text(
-                        "Get to know tea with QualiTea",
-                        // textAlign: TextAlign.center,
-                    
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500,
-                          letterSpacing: 1,
-                          wordSpacing: 2,
-                  color: Color.fromARGB(255, 195, 255, 160),
-                        ),
-                        ),
-                        SizedBox(height: 60),
-                        Material(
-                  color: Color.fromARGB(255, 195, 255, 160),
-                        borderRadius: BorderRadius.circular(10),
-                        child: InkWell(
-                          onTap: (){
-                            Navigator.push(
-                              context, 
+                    color: Color.fromARGB(255, 9, 58, 7),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(70),
+                    )),
+                child: Column(
+                  children: [
+                    SizedBox(height: 12),
+                    Text(
+                      "Get to know tea with QualiTea",
+                      // textAlign: TextAlign.center,
+
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 1,
+                        wordSpacing: 2,
+                        color: Color.fromARGB(255, 195, 255, 160),
+                      ),
+                    ),
+                    SizedBox(height: 60),
+                    Material(
+                      color: Color.fromARGB(255, 195, 255, 160),
+                      borderRadius: BorderRadius.circular(10),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
                               MaterialPageRoute(
-                                builder: (context) => uploadImage()));
-                          },
-                          child: Container(
-                            padding: EdgeInsets.symmetric(
-                              vertical: 15,horizontal: 50),
-                              child: Text(
-                                "Get Start",
-                                style: TextStyle(
-                  color: Color.fromARGB(255, 9, 58, 7),
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                letterSpacing: 1,
-                                ),
-                              ),
-                             
+                                  builder: (context) => uploadImage()));
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 15, horizontal: 50),
+                          child: Text(
+                            "Get Start",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 9, 58, 7),
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 1,
+                            ),
                           ),
                         ),
-
-                        )
-                        
-                    ],
-
-
-                  ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-            
+            ),
           ],
-       ),
-     ),
+        ),
+      ),
     );
   }
 }
