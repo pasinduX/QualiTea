e6ba8758e2e2a4e9e605eaaa30f1e4fdc96e090e
Harini487 <92200258+Harini487@users.noreply.github.com>
2023-03-24 13:26:08 +0530

    Delete _test.dart

--- QualiTea_UI/test/_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:testapp/main.dart';
-
-void main() {
-  testWidgets('MyApp smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-    // Add tests here!
-  });
-}
